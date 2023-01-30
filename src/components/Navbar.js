e58a93524802090fb3e43df920b9846165e2b17c
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-31 00:09:35 +0300

    complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -20,20 +20,10 @@ function Navbar({ setIsLoggedIn }) {
 
   return (
     <div>
-      <NavLink
-        to="/"
-        /* set exact so it knows to only set activeStyle when route is deeply equal to link */
-        exact
-        /* add styling to Navlink */
-        style={linkStyles}
-        /* add prop for activeStyle */
-        activeStyle={{
-          background: "darkblue",
-        }}
-      >
+      <NavLink to="/" exact style={linkStyles}   activeStyle={{    background: "darkblue",  }}>
         Home
-      </NavLink>
-      <NavLink
+        </NavLink>
+        <NavLink
         to="/about"
         exact
         style={linkStyles}
